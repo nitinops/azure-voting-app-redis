a4ecbe9ae2e1fb0d8a9be9a979801c9b49543e2d
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-30 15:20:53 +0000

    Update first_basic_batch.bat

--- first_basic_batch.bat
+++ first_basic_batch.bat
@@ -1,10 +1,8 @@
 @echo off
 SETLOCAL
-CALL :SetValue value1,value2
-echo %value1%
-echo %value2%
+CALL :Display 5 , 10
 EXIT /B %ERRORLEVEL%
-:SetValue
-set "%~1 = 5"
-set "%~2 = 10"
+:Display
+echo The value of parameter 1 is %~1
+echo The value of parameter 2 is %~2
 EXIT /B 0
