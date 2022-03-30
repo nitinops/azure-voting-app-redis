37717418a1f46a8ba6553af7c9c83faff3c8eb9c
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-30 15:38:12 +0000

    Update first_basic_batch.bat

--- first_basic_batch.bat
+++ first_basic_batch.bat
@@ -1,8 +1,7 @@
-@echo off
-SETLOCAL
-CALL :Display 5 , 10
-EXIT /B %ERRORLEVEL%
-:Display
-echo The value of parameter 1 is %~1
-echo The value of parameter 2 is %~2
-EXIT /B 0
+setlocal enableDelayedExpansion
+set WORKSPACE=BEFORE
+(
+  set WORKSPACE=AFTER
+  echo Normal Expansion = %WORKSPACE%
+  echo Delayed Expansion = !WORKSPACE!
+)
